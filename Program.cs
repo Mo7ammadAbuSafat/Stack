fae758fc1592bb7f0303f9552c6e6cd392653395
Mohammad AbuSafat <mo7ammad.abusafat@gmail.com>
2022-11-07 09:25:12 +0200

    use stack in main

--- Program.cs
+++ Program.cs
@@ -4,7 +4,18 @@
     {
         public static void Main(string[] args)
         {
-
+            MyStack<int> stack = new();
+            stack.Push(1);
+            stack.Push(2);
+            stack.Push(3);
+            stack.Push(4);
+            stack.Push(5);
+            stack.Pop();
+            stack.Peek();
+            stack.Print();
+            stack.Clear();
+            stack.Print();
+            
         }
 
     }
